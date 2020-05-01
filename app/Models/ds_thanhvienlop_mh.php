d10b093a65fb8f0ffc5e15633a46034055edb6cf
thienphamstu <52187201+0356581777@users.noreply.github.com>
2020-05-01 09:14:24 +0700

    update source

---
+++ app/Models/ds_thanhvienlop_mh.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ds_thanhvienlop_mh extends Model
+{
+    //
+    protected $table="ds_thanhvienlop_mh";
+    const UPDATED_AT = null;
+
+    const CREATED_AT=null;
+}
